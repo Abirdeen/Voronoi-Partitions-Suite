4b9db5a15b338e608ecbb56dcea484f5ae3c6a47
Abirdeen <102402217+Abirdeen@users.noreply.github.com>
2025-02-07 16:57:13 +0000

    FEAT: Add nearest neighbour alg to MetricSpace
    The nearest neighbour algorithm finds the closest k points to a given
    point. This should only use the distance method rather than details of
    the metric space, so can be implemented in the abstract base class. The
    k=1 case is useful for studying voronoi cells.

--- metric_spaces.py
+++ metric_spaces.py
@@ -26,7 +26,31 @@ class MetricSpace(ABC, Generic[PointType]):
     @abstractmethod
     def distance(self, point1, point2) -> float:
         raise NotImplementedError
- 
+    
+    def nearest_neighbours(self, points: Iterable[PointType], givenPoint: PointType, k: int) -> Iterable[PointType]:
+        '''Find the `k` nearest neighbours of a given point.
+        
+        Parameters
+        ----------
+        points: Iterable[PointType]
+            Iterable of points in space.
+        givenPoint : PointType
+            Single point in space, whose nearest neighbours you want to find.
+        k : int
+            Number of nearest neighbours to find.
+        
+        Returns
+        -------
+        Iterable[PointType]
+            The `k` nearest neighbours of `givenPoint`.
+            
+        Examples
+        --------
+        >>> ???
+        
+        '''
+        raise NotImplementedError
+
 if __name__ == '__main__':
 
     pass
